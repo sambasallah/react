37f9f3eafd87db57f347357b294f8881c227491a
Dan <dan.abramov@gmail.com>
2019-04-30 10:36:12 +0100

    Always resolve to latest type

--- packages/react-reconciler/src/ReactFiberHotReload.js
+++ packages/react-reconciler/src/ReactFiberHotReload.js
@@ -10,28 +10,23 @@
 import type {ReactElement} from 'shared/ReactElementType';
 import type {Fiber} from './ReactFiber';
 
-// TODO: this leaks
-let latestTypeByKey = new Map();
-
 export function acceptsTypeWithHotReload(fiber: Fiber, element: ReactElement): boolean {
   const fiberElementType = fiber.elementType;
   const elementType = element.type;
   if (
-    fiberElementType.__key !== undefined &&
-    fiberElementType.__key === elementType.__key
+    fiberElementType.__debugIdentity !== undefined &&
+    fiberElementType.__debugIdentity === elementType.__debugIdentity
   ) {
-    // TODO: we don't actually know which one is latest.
-    latestTypeByKey.set(fiberElementType.__key, elementType);
     return true;
   }
   return false;
 }
 
 export function resolveTypeWithHotReload(type: Function): Function {
-  const key = type.__key;
-  if (key === undefined) {
+  const debugIdentity = type.__debugIdentity;
+  if (debugIdentity === undefined) {
     return type;
   }
-  return latestTypeByKey.get(key) || type;
+  return debugIdentity.current;
 }
 
